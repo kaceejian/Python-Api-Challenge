f8b683911ccf5304c8404d30e885b83330214780
kc chien <kaceejian@gmail.com>
2021-09-19 16:45:12 -0500

    add files

---
+++ starter_code/ARCHIVED/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "ebf7377c80a9c88f36d20bc316cc7740"
+
+# Google API Key
+g_key = "YOUR KEY HERE!"
--- starter_code/my_keys.py
+++ starter_code/my_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "ebf7377c80a9c88f36d20bc316cc7740"
 
 # Google API Key
 g_key = "YOUR KEY HERE!"
